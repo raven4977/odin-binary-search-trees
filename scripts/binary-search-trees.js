ee24ba9b57ca468992eec76f7e826636003dfed8
Cameron Brown <moontsukuyomi6@gmail.com>
2025-08-20 00:01:43 +0200

    Add: add insert function to handle inserting a value into tree

--- scripts/binary-search-trees.js
+++ scripts/binary-search-trees.js
@@ -7,9 +7,28 @@ class Node {
 }
 
 class Tree {
-  constructor (array) {
+  constructor(array) {
     this.root = buildTree(sortArray(array));
   }
+
+  insert(current = this.root, value) {
+    if (!current) {
+      return new Node(value);
+    }
+
+    if (current.data === value) {
+      return current;
+    }
+
+    if (value < current.data) {
+      current.left = this.insert(current.left, value);
+    } else if (value > current.data) {
+      current.right = this.insert(current.right, value);
+    }
+    return current;
+  }
+
+  deleteItem(value) {}
 }
 
 function buildTree(array, start = 0, end = array.length - 1) {
@@ -23,10 +42,6 @@ function buildTree(array, start = 0, end = array.length - 1) {
 
 function sortArray(array) {
   return [...new Set(array)].sort((a, b) => a - b);
-}
-
-function sortedArrayToBST(array) {
-  return buildTree(array, 0, array.length - 1);
 }
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
@@ -45,4 +60,13 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const tree = new Tree(array);
 
+const testArr = [0, 1, 2, 3, 4, 5, 6, 7];
+
+tree.insert(tree.root, 14);
+
 prettyPrint(tree.root);
+
+// tree.deleteItem();
+
+const test = new Tree(testArr);
+// prettyPrint(test.root);
