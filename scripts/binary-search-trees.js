1624778e826b38f6460ec1d6c6f7e63452755b05
Cameron Brown <moontsukuyomi6@gmail.com>
2025-08-19 19:19:16 +0200

    Add: add buildTree function to handle turning an array into a balanced BST, add sortArray function to handle sorting and removing duplicate values and update Tree class constructor parameters and attributes

--- scripts/binary-search-trees.js
+++ scripts/binary-search-trees.js
@@ -7,8 +7,42 @@ class Node {
 }
 
 class Tree {
-  constructor(array, root) {
-    this.array = array;
-    this.root = root;
+  constructor (array) {
+    this.root = buildTree(sortArray(array));
   }
 }
+
+function buildTree(array, start = 0, end = array.length - 1) {
+  if (start > end) return null;
+  let mid = start + Math.floor((end - start) / 2);
+  let root = new Node(array[mid]);
+  root.left = buildTree(array, start, mid - 1);
+  root.right = buildTree(array, mid + 1, end);
+  return root;
+}
+
+function sortArray(array) {
+  return [...new Set(array)].sort((a, b) => a - b);
+}
+
+function sortedArrayToBST(array) {
+  return buildTree(array, 0, array.length - 1);
+}
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const tree = new Tree(array);
+
+prettyPrint(tree.root);
