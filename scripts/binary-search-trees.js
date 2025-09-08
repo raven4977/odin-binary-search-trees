f7675b896d6d113622fc5e1fe492a3040efe94e7
Cameron Brown <moontsukuyomi6@gmail.com>
2025-09-08 19:14:33 +0200

    Add: add a function to handle creating a BST

--- scripts/binary-search-trees.js
+++ scripts/binary-search-trees.js
@@ -12,3 +12,14 @@ class Tree {
     this.root = buildTree(arr, 0, arr.length - 1);
   }
 }
+
+function buildTree(arr, start, end) {
+  if (start > end) return null;
+  const mid = start + Math.floor((end - start) / 2);
+  const root = new Node(arr[mid]);
+  root.left = buildTree(arr, start, mid - 1);
+  root.right = buildTree(arr, mid + 1, end);
+  return root;
+}
+
+const array = [1, 2, 3, 4, 5, 6, 7];
